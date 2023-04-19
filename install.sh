6d59e075f057efe95258298b5a9399565e862aa1
jswent <94636565+jswent@users.noreply.github.com>
2023-04-19 16:34:52 -0400

    initial commit

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Elevate privileges
+
+sudo -v
+
+# Install apps from macapps.link
+
+curl -s 'https://api.macapps.link/en/firefox-chrome-vscode-iterm-slack-whatsapp' | sh
+
+# Install brew
+
+command -v brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install brew packages
+
+packages="gnu-sed cmake adobe-acrobat-reader microsoft-office node neovim"
+
+for package in $packages; do
+  brew install $package
+done
+
+# Reset sudo timestamp to expire immediately after the script finishes
+sudo -k
